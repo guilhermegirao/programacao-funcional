0a3f0497ae218955a4a433767f0f8d3f3ac6e33b
guilhermegirao <guigirao@hotmail.com>
2021-05-28 09:37:40 -0300

    fix: fix length precedence in final function

--- atividade-3/005_final/005_final.hs
+++ atividade-3/005_final/005_final.hs
@@ -3,7 +3,7 @@ module Main where
 -- Defina a função final tal que final xs é uma lista formada pelos n elementos finais de xs.
 
 final :: Int -> [Int] -> [Int]
-final n xs = drop $length xs n xs
+final n xs = drop (length xs - n) xs
 
 main = do
   a <- readLn :: IO Int
